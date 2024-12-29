e66d6cbcf8ee2e145335dfccd5910dbfabf893d8
anujpant08 <anujpant2@gmail.com>
2024-12-29 18:56:57 +0530

    Initial Commit

---
+++ NoSleep/NoSleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoSleepApp.swift
+//  NoSleep
+//
+//  Created by Anuj Pant on 29/12/24.
+//
+
+import SwiftUI
+
+@main
+struct NoSleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
