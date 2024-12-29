b65ebc3554c5405c198cd91cd56c9fd7d0a61141
anujpant08 <anujpant2@gmail.com>
2024-12-29 21:21:23 +0530

    Setup UI

--- NoSleep/ContentView.swift
+++ NoSleep/ContentView.swift
@@ -8,12 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var buttonPressed: Bool = false
+    @State var textValue: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField(
+                "Enter a value",
+                text: self.$textValue
+            )
+            .padding(.all)
+            Text(
+                "Enter a value less than your system's sleep timer"
+            )
+            .padding(.init(top: 0, leading: 0, bottom: 5, trailing: 0))
+            Text(
+                "Please keep the app running in background"
+            )
+            .colorMultiply(.secondary)
+            .padding(.bottom)
+            Button(
+                "Set No Sleep Limit"
+            ){
+                print("Setting no sleep limit")
+                buttonPressed = true
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.accentColor)
+            if (buttonPressed) {
+                Text("No sleep limit set to " + textValue)
+                // Logic to avoid sleep...
+            }
         }
         .padding()
     }
